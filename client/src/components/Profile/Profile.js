01edd2506fe70bf5ebd6abaa454d9acb549655e3
MaxJeong <max.jeong@mail.utoronto.ca>
2021-05-13 00:47:46 -0400

    display more user details on their profile page

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
 import { connect } from 'react-redux';
 import { 
 	getPostsByUserId,
@@ -8,7 +11,45 @@ import {
 import Post from '../Posts/Post';
 import LoadingPosts from '../Posts/LoadingPosts';
 
-const styles = {}
+const styles = {
+    paper: {
+		padding: 10
+	},
+
+	username: {
+
+	},
+
+	email: {
+		color: '#888',
+		marginBottom: 10
+	},
+
+	detailsBlock: {
+		display: 'flex'
+	},
+
+	detail: {
+		marginRight: 5,
+		fontWeight: 'bold'
+	},
+
+	detailTitle: {
+		marginLeft: 3,
+		textTransform: 'uppercase',
+		fontSize: 10,
+		fontWeight: 'normal'
+	},
+
+	btnBlock: {
+		width: '100%',
+		textAlign: 'right'
+	},
+
+	btnFollow: {
+
+	}
+}
 
 class Profile extends Component {
 
@@ -33,10 +74,58 @@ class Profile extends Component {
 		} = this.props
 
         let items;
+        let profileInfo;
+        let followBtns;
 		items = list && list.map(el => <Post key={el._id} post={el}/>);
+
+        /*
+        If the current user is authenticated and didn't follow the user
+        on their profile page, show the follow button
+        */
+        if (auth.isAuthenticated) {
+            if (user.following.indexOf(this.props.match.params.userId) === -1) {
+                followBtns = (<div className={classes.btnBlock}>
+                    <Button variant='outlined' className={classes.btnFollow}>
+                        Follow
+                    </Button>
+                </div>)
+            } else {
+                followBtns = (<div className={classes.btnBlock}>
+                    <Button variant='outlined' className={classes.btnFollow}>
+                        Unfollow
+                    </Button>
+                </div>)
+            }
+        }
+
+        //If profile and items are not null, retrieve user information
+        if (profile && items) {
+            profileInfo = (
+				<Paper className={classes.paper}>
+                    <h1 className={classes.username}>{profile.username}</h1>
+                    <div className={classes.email}>{profile.email}</div>
+                    <div className={classes.detailsBlock}>
+                        <div className={classes.detail}>
+                            {items.length}
+                            <span className={classes.detailTitle}>Posts</span>
+                        </div>
+                        <div className={classes.detail}>
+                            {profile.followers.length}
+                            <span className={classes.detailTitle}>Followers</span>
+                        </div>
+                        <div className={classes.detail}>
+                            {profile.following.length}
+                            <span className={classes.detailTitle}>Following</span>
+                        </div>
+                        { followBtns }
+                    </div>
+                </Paper>
+			)
+        }
 
         return (
             <div>
+                { loadingProfile ? <div>Loading</div> : profileInfo }
                 { loadingPosts ? <LoadingPosts /> : items }
             </div>
         )
